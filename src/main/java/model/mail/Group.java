b479e7abcd619a55be7f21498164ab613ba220cf
arbroween <arbroween@gmail.com>
2021-04-16 12:09:22 +0200

    Implémenté classes Person et Group

--- src/main/java/model/mail/Group.java
+++ src/main/java/model/mail/Group.java
@@ -1,5 +1,26 @@
 package model.mail;
 
-public class Group {
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
+public class Group {
+    private Person sender;
+    private List<Person> recipients = new ArrayList<>();
+
+    public Group(Person sender) {
+        this.sender = sender;
+    }
+
+    public Person getSender() {
+        return sender;
+    }
+
+    public void addRecipient(Person recipient) {
+        recipients.add(recipient);
+    }
+
+    public Iterator<Person> recipients() {
+        return recipients.iterator();
+    }
 }
